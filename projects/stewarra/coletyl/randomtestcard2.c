a12a247ebf60027ca58429bae975326e8c3564f8
racestewart <35312415+racestewart@users.noreply.github.com>
2019-06-02 21:51:53 -0700

    added a4 tests

---
+++ projects/stewarra/coletyl/randomtestcard2.c
@@ -0,0 +1,66 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <string.h>
+
+int main() {
+  srand(time(NULL));
+
+  int i, j, l, m;
+  int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0, player = 0, passed = 0, failed = 0;
+  struct gameState G, test;
+  int seed = 1000;
+  int numPlayers = 2;
+  int k[10] = {adventurer, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy, council_room};
+
+  printf("\n Testing Great Hall Card\n");
+
+  for(i = 0; i < 100; i++) {
+    initializeGame(numPlayers, k, seed, &G);
+
+    memcpy(&test, %G, sizeof(struct gameState));
+
+    ghFunction(&G, handpos)
+
+    player = whoseTurn(&test);
+
+    // check hand count
+    if(G.hand[player][test.handCount[player] - 1] != -1) {
+      passed++;
+    } else {
+      failed++;
+    }
+
+    // check if card was played
+    if(G.playedCardCount == test.playedCardCount + 1) {
+      passed++;
+    } else {
+      failed++;
+    }
+
+    // check if num actions is accurate
+    if(G.numActions == test.numActions + 1) {
+      passed++;
+    } else {
+      failed++;
+    }
+
+    // check if discard is accurate
+    if(G.discardCount[player] == test.discardCount[player]) {
+      passed++;
+    } else {
+      failed++;
+    }
+  }
+
+  printf("Number of tests passed: %d\n", passed);
+  printf("Number of tests failed: %d\n", failed);
+  printf("End of Great Hall Card Test\n");
+
+  return 0;
+}
